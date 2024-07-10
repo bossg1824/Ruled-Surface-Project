905a8985e125d94bae9ac30c82b400aeb686edb2
Gunderson <gunderso@grinnell.edu>
2024-07-10 14:08:20 -0500

    Developable Surface intersector completed, going to fix pedistal and small things on grabbable ruled surface generator

--- Assets/Scripts/DevelopableSurfaceIntersector.cs
+++ Assets/Scripts/DevelopableSurfaceIntersector.cs
@@ -18,9 +18,15 @@ public class DevelopableSurfaceIntersector : MonoBehaviour
     [SerializeField]
     public Color HighlightColor;
 
+    [SerializeField]
+    public float extraRadius;
+
+    [SerializeField]
+    public float AllowedDegreeOffset;
     private Dictionary<Transform, Transform> intersectingRulings;
     private string serial;
     private Color oldColorMA;
+    private bool oldColorFound = false;
     //for debugging
     //private Transform putThatThingBackWhereItCameFromOrSoHelpMe;
 
@@ -28,6 +34,7 @@ public class DevelopableSurfaceIntersector : MonoBehaviour
     void Start()
     {
         intersectingRulings = new Dictionary<Transform, Transform>();
+        PointLinePrefab.GetComponent<Renderer>().material.color = HighlightColor;
 
     }
 
@@ -39,29 +46,44 @@ public class DevelopableSurfaceIntersector : MonoBehaviour
 
         foreach(KeyValuePair<Transform, Transform> pair in intersectingRulings)
         {
-            Ray rayToFindIntersectionPoint = new Ray(pair.Key.position, pair.Key.rotation * Vector3.up);
+            Vector3 RayNormals = pair.Key.rotation * Vector3.up; 
+            Ray rayToFindIntersectionPoint = new Ray(pair.Key.position, RayNormals);
             float distanceFromCenter;
             planeForEachIntersection.Raycast(rayToFindIntersectionPoint, out distanceFromCenter);
             Vector3 positionForIntersection = rayToFindIntersectionPoint.GetPoint(distanceFromCenter);
 
-            if (false)
+            if (CheckIfParallel(rayToFindIntersectionPoint))
             {
-                Material WorkingWith = pair.Key.GetComponent<Material>();
-                oldColorMA = WorkingWith.color;
+                Material WorkingWith = pair.Key.GetComponent<Renderer>().material;
                 WorkingWith.color = HighlightColor;
-                pair.Value.GetComponent<MeshRenderer>().enabled = false;
+                pair.Value.GetComponent<Renderer>().enabled = false;
             } else
             {
-                pair.Value.GetComponent<MeshRenderer>().enabled = true;
+                pair.Value.GetComponent<Renderer>().enabled = true;
                 pair.Value.position = positionForIntersection;
                 pair.Value.rotation = transform.rotation;
-               // pair.Key.GetComponent<Material>().color = oldColorMA;
+                pair.Key.GetComponent<Renderer>().material.color = oldColorMA;
+
+
+                Ray rayForXScale = new Ray(pair.Key.position +  pair.Key.rotation * new Vector3(pair.Key.localScale.x/2, 0, 0), RayNormals);
+                planeForEachIntersection.Raycast(rayForXScale, out float distanceFromX);
+                float XScale = Vector3.Distance(positionForIntersection, rayForXScale.GetPoint(distanceFromX));
+
+                Ray rayForZScale = new Ray(pair.Key.position + pair.Key.rotation * new Vector3(0, 0, pair.Key.localScale.z / 2), RayNormals);
+                planeForEachIntersection.Raycast(rayForZScale, out float distanceFromZ);
+                float ZScale = Vector3.Distance(positionForIntersection, rayForZScale.GetPoint(distanceFromZ));
+
+                pair.Value.localScale = new Vector3(XScale * 2 + extraRadius, Thickness, ZScale * 2 + extraRadius);
             }
             
             //Pair.Value
             }
     }
 
+    private bool CheckIfParallel(Ray line)
+    {
+        return (Mathf.Abs(90 - Vector3.Angle(transform.rotation * Vector3.up, line.direction)) < AllowedDegreeOffset);
+    }
     private void OnTriggerEnter(Collider other)
     {
 
@@ -69,8 +91,15 @@ public class DevelopableSurfaceIntersector : MonoBehaviour
         {
             Transform newPoint = Instantiate(PointLinePrefab, transform);
             //figure out other setup stuff
-            newPoint.localScale = new Vector3 (other.transform.localScale.x * (float) 1.1, Thickness, other.transform.localScale.z * (float) 1.1);
+            newPoint.localScale = new Vector3 (other.transform.localScale.x + extraRadius, Thickness, other.transform.localScale.z + extraRadius);
+            newPoint.GetComponent<Renderer>().material.color = HighlightColor;
             intersectingRulings.Add(other.transform, newPoint);
+            if (!oldColorFound)
+            {
+                Debug.Log("Changing Color");
+                oldColorMA = other.GetComponent<Renderer>().material.color;
+                oldColorFound = true;
+            }
         }
     }
 
@@ -82,6 +111,7 @@ public class DevelopableSurfaceIntersector : MonoBehaviour
             intersectingRulings.TryGetValue(other.transform, out pointValue);
             Destroy(pointValue.gameObject);
             intersectingRulings.Remove(other.transform);
+            other.GetComponent<Renderer>().material.color = oldColorMA;
         }
     }
 }
