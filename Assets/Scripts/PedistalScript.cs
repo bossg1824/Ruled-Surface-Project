41fc4d1f4e17311982247de17fc1f814a5796fde
Gunderson <gunderso@grinnell.edu>
2024-07-16 10:48:22 -0500

    Intro scene progress, added pictures in posters, need to fix collision issues in other intro scene

--- Assets/Scripts/PedistalScript.cs
+++ Assets/Scripts/PedistalScript.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44ff4e678bb6f5df410c39055bee63794418e411c80befab4d4e04988a29b9bc
-size 5846
+oid sha256:20dc22c2c59959ea3a2f393361155bd410b5a408c24e658a23119ce8e6c704b0
+size 5847
