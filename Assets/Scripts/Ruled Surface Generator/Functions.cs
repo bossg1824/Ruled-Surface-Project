1fa81337d60e87238b9ebd2c6dbc042615d40d33
Gunderson <gunderso@grinnell.edu>
2024-07-26 17:59:13 -0500

    Working out surfaces, need to get the last few to work, work on developable surface scene, and time the cubics and quartics scene

--- Assets/Scripts/Ruled Surface Generator/Functions.cs
+++ Assets/Scripts/Ruled Surface Generator/Functions.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b170b7213a1b9b180ba3ff7c4ef2db8ddfcdd11686e008b9b91601af5c6119b0
-size 7294
+oid sha256:0aa84b8268ad7797587541c603f6abdacc07ab777c6baaed7a576466f9a23488
+size 16635
